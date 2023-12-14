b9449b8522e5b515440d92e23cce8c02b62ae815
lavac <lavac@foxmail.com>
2023-12-14 16:58:07 +0800

    test:pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import unocss from "@unocss/astro";
+import AstroPWA from "@vite-pwa/astro";
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    AstroPWA({
+      injectRegister: true,
+    }),
     solidJs(),
     unocss({
       injectReset: true,
